b5c279af6f50e95b2dcd87f77996ac761513ae61
尤柯 <changhong.xch@alibaba-inc.com>
2023-02-03 15:59:01 +0800

    demo

---
+++ demo/src/main/java/com/google/guava/Main.java
@@ -0,0 +1,13 @@
+package main.java.com.google.guava;
+
+/**
+ * ...
+ *
+ * @author youke.xch
+ * @since ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
